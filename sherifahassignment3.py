5e32c2be1199de0c349cd53dbe85282acee292df
Oyinkansola Ojelade <oyinkansolaojelade1@gmail.com>
2022-07-06 20:58:17 +0100

    commit

--- sherifahassignment3.py
+++ sherifahassignment3.py
@@ -3,3 +3,17 @@ num=8890.892002
 two_decimals=round(num,2)
 
 print(two_decimals)
+
+#assignmant no2
+import cmath
+a=float(input('enter a:'))
+b=float(input('enter b:'))
+c=float(input('enter c:'))
+
+# calculate the discriminat
+d=(b**2) - (4*a*c)
+
+# find two solutions 
+sol1=(-b-cmath.sqrt(d))/(2*a)
+sol2=(-b+cmath.sqrt(d))/(2*a)
+print(("the solution are {0} and {1}".format(sol1,sol2)))
