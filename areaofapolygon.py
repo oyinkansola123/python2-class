87e299a7623b43ed5661ab648c7ed10a419d1fc9
Oyinkansola Ojelade <oyinkansolaojelade1@gmail.com>
2022-07-08 14:24:09 +0100

    commit

---
+++ areaofapolygon.py
@@ -0,0 +1,4 @@
+a=int(input("Enter apothem:"))
+p=int(input("Enter perimeter"))
+area=1/2*(a*p)
+print(area)
--- sherifahassignment3.py
+++ sherifahassignment3.py
@@ -5,15 +5,17 @@ two_decimals=round(num,2)
 print(two_decimals)
 
 #assignmant no2
-import cmath
-a=float(input('enter a:'))
-b=float(input('enter b:'))
-c=float(input('enter c:'))
+# import cmath
+# a=float(input('enter a:'))
+# b=float(input('enter b:'))
+# c=float(input('enter c:'))
 
-# calculate the discriminat
-d=(b**2) - (4*a*c)
+# # calculate the discriminat
+# d=(b**2) - (4*a*c)
 
-# find two solutions 
-sol1=(-b-cmath.sqrt(d))/(2*a)
-sol2=(-b+cmath.sqrt(d))/(2*a)
-print(("the solution are {0} and {1}".format(sol1,sol2)))
+# # find two solutions 
+# sol1=(-b-cmath.sqrt(d))/(2*a)
+# sol2=(-b+cmath.sqrt(d))/(2*a)
+# print(("the solution are {0} and {1}".format(sol1,sol2)
+
+
