bf71ffed404634b4ceb19478599e305ae9240264
Oyinkansola Ojelade <oyinkansolaojelade1@gmail.com>
2022-07-16 20:58:14 +0100

    commit

--- areaofapolygon.py
+++ areaofapolygon.py
@@ -1,4 +1,4 @@
-a=int(input("Enter apothem:"))
-p=int(input("Enter perimeter"))
-area=1/2*(a*p)
-print(area)
+# a=int(input("Enter apothem:"))
+# p=int(input("Enter perimeter"))
+# area=1/2*(a*p)
+# print(area)
--- sherifahassignment2.py
+++ sherifahassignment2.py
@@ -12,15 +12,3 @@ for guessestaken in range(1,6):
     else:
         break
 print('the number i was thinking was', str(RandomNumber))
-
-
-
-
-b_num=list(input("input a binary number:"))
-value=0
-
-for i in range(len(b_num)):
-    digit=b_num.pop()
-    if digit=='1':
-        value=value + pow(2,i)
-        print ("The decimal value of the number is",value)
